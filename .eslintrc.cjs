14e78dfbddc02b4d746c5a9545182fb9c6cc2dba
Erik Craddock <erik@craddock.org>
2023-11-19 19:23:15 -0600

    feat: updates logo, fixes lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,40 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: [
+      'tsconfig.json',
+      'tsconfig.node.json',
+    ]
+  },
+  plugins: [
+    'react', '@typescript-eslint', 'prettier',
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react/react-in-jsx-scope': 0,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
     ],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 'off', 
   },
 }
